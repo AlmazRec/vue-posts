b22337a8ae2dd31b698ee0bb7558117efd949e27
Almaz Bogdanov <al.bogdanov@internet.ru>
2025-01-26 15:14:31 +0500

    Update form

--- src/App.vue
+++ src/App.vue
@@ -21,39 +21,94 @@ function addPost() {
 
 <template>
     <div class="container">
-        <div class="box"  v-for="post in posts" :key="post">
+        <div class="form-container">
+            <div class="form-post">
+                <input v-model="newPost" type="text" placeholder="название...">
+                <button @click="addPost" type="submit">добавить</button>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="content">
+        <div class="posts"  v-for="post in posts" :key="post">
             <h1> {{ post.id }} </h1>
             <p> {{ post.text }} </p>
         </div>
     </div>
 
-    <div class="post-form">
-        <p>Форма поста</p>
-        <input v-model="newPost" type="text">
-        <button @click="addPost" type="submit">Add</button>
-    </div>
-
-
 </template>
 
 <style scoped>
+     input {
+         width: 280px;
+         height: 33px;
+         background-color: #f6e4c1;
+         border: none;
+         outline: none;
+         border-radius: 50px;
+         padding-left: 10px;
+         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+     }
+     button {
+         display: inline-block;
+         margin-top: 14px;
+         width: 140px;
+         height: 34px;
+         font-family: 'Helvetica', 'Arial', sans-serif;
+         text-align: center;
+         font-size: 17px;
+         padding: 0;
+         line-height: 30px;
+         background-color: #ffb74d;
+         color: #fff;
+         border-radius: 7px;
+         border: none;
+         cursor: pointer;
+         position: relative;
+         transition: transform ease-in 0.1s, background-color ease-in 0.25s;
+         box-shadow: 0 2px 25px rgba(255, 183, 77, 0.7);
+     }
 
-    .post-form {
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 300px;
-        height: 300px;
-        display: flex;
-
-    }
+     button:active {
+         transform: scale(0.9);
+         background-color: #ffb74d; /* Темнее на 5% */
+         box-shadow: 0 2px 25px rgba(255, 0, 130, 0.2);
+     }
 
     .container {
         display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 330px;
+    }
+    .form-container {
+        display: flex;
         justify-content: center;
         align-items: center;
+        width: 380px;
+        height: 330px;
+        border-radius: 18px;
+
     }
-    .box {
+    .form-post {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 300px;
+        height: 300px;
+
+    }
+
+    .content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+    .posts {
         margin: 10px;
         display: flex;
         background-color: greenyellow;
